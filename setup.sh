9f9b424afa47da2bea04a29284ce3ea96df0c366
RicardoStephen <rls454@cornell.edu>
2016-09-05 15:02:53 -0400

    update setup for variable filename

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
+# track current directory
+DIR=`pwd`
+
 # create .trash if required
 mkdir -p ~/.trash
 
 # create ./old
-mkdir -p ./old
+mkdir -p $DIR/old
 
 # config files
 files=(.bash_profile .bashrc .bash_aliases .screenrc .emacs.d)
@@ -10,7 +13,7 @@ files=(.bash_profile .bashrc .bash_aliases .screenrc .emacs.d)
 for f in "${files[@]}"
 do
     # move old config files to ./old
-    mv ~/$f ./old
+    mv ~/$f $DIR/old
     # create symbolic link to the new config file
-    ln -sb ~/debian_config/$f ~
+    ln -sb $DIR/$f ~
 done
